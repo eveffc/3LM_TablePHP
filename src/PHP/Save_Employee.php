07c6d7875e216a95f4dd5a8eed09255fe01f9dc6
Evelyn Santos <117581658+eveffc@users.noreply.github.com>
2023-10-02 22:13:39 -0300

    .

--- src/PHP/Save_Employee.php
+++ src/PHP/Save_Employee.php
@@ -10,13 +10,11 @@ $salario = filter_input(INPUT_POST, 'salario');
 
 // Verifique se o campo oculto 'id' foi enviado
 if (isset($_POST['id'])) {
-    // É uma atualização, obtenha o ID do funcionário
+
     $id = $_POST['id'];
 
-    // Construa a query de UPDATE
     $query = "UPDATE funcionarios SET nome='$nome', sobrenome='$sobrenome', cargo='$cargo', nascimento='$nascimento', salario='$salario' WHERE id='$id'";
 } else {
-    // É uma inserção, construa a query de INSERT
     $query = "INSERT INTO funcionarios (nome, sobrenome, cargo, nascimento, salario, modified) VALUES ('$nome', '$sobrenome', '$cargo', '$nascimento', '$salario', NOW())";
 }
 
--- src/PHP/Save_Office.php
+++ src/PHP/Save_Office.php
@@ -7,13 +7,10 @@ $descricao = filter_input(INPUT_POST, 'descricao');
 
 // Verifique se o campo oculto 'id' foi enviado
 if (isset($_POST['id'])) {
-    // É uma atualização, obtenha o ID do funcionário
     $id = $_POST['id'];
 
-    // Construa a query de UPDATE
     $query = "UPDATE cargos SET cargo='$cargo', descricao='$descricao' WHERE id='$id'";
 } else {
-    // É uma inserção, construa a query de INSERT
     $query = "INSERT INTO funcionarios (cargo, descricao, modified) VALUES ('$cargo', '$descricao', NOW())";
 }
 
